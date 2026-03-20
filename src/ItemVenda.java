34a13ae9ae15c2a1dab51a07529b2d5d8a7debef
Guilherme Vicentini <guii.vicentini@hotmail.com>
2026-03-20 19:14:12 -0300

    feat: create ItemVenda, Venda and simulation through Main class

--- src/ItemVenda.java
+++ src/ItemVenda.java
@@ -9,5 +9,27 @@ public class ItemVenda {
     private BigDecimal precoUnitario;
     private BigDecimal subtotal;
 
+    public ItemVenda(Venda venda, Produto produto, Integer quantidade) {
+        if(quantidade <= 0) {
+            throw new IllegalArgumentException("Quantidade inválida");
+        }
 
+        this.venda = venda;
+        this.produto = produto;
+        this.quantidade = quantidade;
+        this.precoUnitario = produto.getPreco();
+        this.subtotal = precoUnitario.multiply(BigDecimal.valueOf(quantidade));
+    }
+
+    public BigDecimal getSubtotal() {
+        return subtotal;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
 }
--- src/Venda.java
+++ src/Venda.java
@@ -12,5 +12,51 @@ public class Venda {
     private BigDecimal valorTotal;
     private List<ItemVenda> itens;
 
+    public Venda (Cliente cliente, Usuario usuario) {
+        if (!usuario.isAtivo()) {
+            throw new IllegalArgumentException("Usuário inativo");
+        }
+        this.cliente = cliente;
+        this.usuario = usuario;
+        this.data = LocalDateTime.now();
+        this.itens = new ArrayList<>();
+        this.valorTotal = BigDecimal.ZERO;
+    }
 
+    public void adicionarItem(Estoque estoque, int quantidade) {
+        if (!estoque.temDisponivel(quantidade)) {
+            throw new IllegalArgumentException("Estoque insuficiente");
+        }
+
+        estoque.reduzir(quantidade);
+
+        ItemVenda item = new ItemVenda(this, estoque.getProduto(), quantidade);
+        itens.add(item);
+
+        recalcularTotal();
+    }
+
+    private void recalcularTotal() {
+        this.valorTotal = itens.stream()
+                .map(ItemVenda::getSubtotal)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+//    private void recalcularTotal() {
+//        BigDecimal total = BigDecimal.ZERO;
+//
+//        for (ItemVenda item : itens) {
+//            total = total.add(item.getSubtotal());
+//        }
+//
+//        this.valorTotal = total;
+//    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public List<ItemVenda> getItens() {
+        return itens;
+    }
 }
